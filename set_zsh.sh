0faee7b6be4b2c63924a6969e86b6137d87fe448
John.G <98368355+JohnG-mit@users.noreply.github.com>
2025-11-12 23:39:35 +0800

    Create set_zsh.sh to install Oh My Zsh and plugins

---
+++ set_zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh && cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
